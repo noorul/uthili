07accf816fe338e1d94272fd78fe05402626658b
Noorul Islam K M <noorul@noorul.com>
2011-03-05 22:48:29 +0530

    Add initial set of files

---
+++ footer.php
@@ -0,0 +1,18 @@
+	</div><!-- #main -->
+	
+	<div id="footer">
+		<div id="colophon">
+		
+			<div id="site-info">
+				<p>Powered by <span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'uthili' ) ?>" rel="generator"><?php _e( 'WordPress', 'uthili' ) ?></a></span> and <span id="theme-link"><a href="http://themeuthilis.com/uthili/" title="<?php _e( 'The Uthili Theme for WordPress', 'uthili' ) ?>" rel="designer"><?php _e( 'Uthili', 'uthili' ) ?></a></span>.</p>			
+			</div><!-- #site-info -->
+			
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+	
+</div><!-- #wrapper -->	
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
