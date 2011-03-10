18c9967e4c3cee1f48f2690e900835f04a82a518
Noorul Islam K M <noorul@noorul.com>
2011-03-10 12:39:06 +0530

    Add friendfeed.php

---
+++ friendfeed.php
@@ -0,0 +1,6 @@
+<!-- Friendfeed Badge -->
+
+<script type="text/javascript" src="http://friendfeed.com/embed/badge/noorul?width=200"></script><noscript><a href="http://friendfeed.com/noorul"><img alt="View my FriendFeed" style="border:0;" src="http://friendfeed.com/embed/badge/noorul?width=200&amp;format=png"/></a></noscript>
+
+<!-- Friendfeed Entrie -->
+<script type="text/javascript" src="http://friendfeed.com/embed/widget/noorul?v=3&amp;width=200"></script><noscript><a href="http://friendfeed.com/noorul"><img alt="View my FriendFeed" style="border:0;" src="http://friendfeed.com/embed/widget/noorul?v=3&amp;width=200&amp;format=png"/></a></noscript>
