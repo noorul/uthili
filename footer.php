8378070e87cc22e1950621252cb2f8fb6c3b99ed
Noorul Islam K M <noorul@noorul.com>
2011-09-11 10:12:51 +0530

    * footer.php: Add copyright information.

--- footer.php
+++ footer.php
@@ -1,6 +1,11 @@
 	</div><!-- #main -->
 	
 	<div id="footer">
+<br>
+The posts on this blog are provided ‘as is’ with no warranties and confer no rights. The opinions expressed on this site are my own and do not necessarily represent my company's positions, strategies, or opinions.
+Copyright © 2007-2011 Noorul Islam K M (noorul@noorul.com). Please feel free to reuse content under a Creative Commons Attribution Sharealike license unless otherwise noted.
+<br><br>
+
 		<div id="colophon">
 		
 			<div id="site-info">
